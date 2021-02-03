acebd1e84f5fcbf518ac6e0535a38bf990bfcd58
jonasbinotti <jonasbinotti@gmail.com>
2021-02-02 19:01:33 -0500

    update test.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(),Minha Primeira edição com o GIT DevOps .)
+        self.assertRegex(result.data.decode()Minha Primeira edição com o GIT DevOps .)
 
 
 if __name__ == "__main__":
